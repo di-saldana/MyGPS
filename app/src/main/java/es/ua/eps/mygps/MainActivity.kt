9d07d453a65e2245df0a1708726b9999ccfd5694
di-saldana <dsl42@alu.ua.es>
2023-10-19 20:11:25 +0200

    Added main implementation

--- app/src/main/java/es/ua/eps/mygps/MainActivity.kt
+++ app/src/main/java/es/ua/eps/mygps/MainActivity.kt
@@ -1,11 +1,78 @@
 package es.ua.eps.mygps
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var locationTextView: TextView
+    private lateinit var lonResText: TextView
+    private lateinit var latResText: TextView
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        locationTextView = findViewById(R.id.locationText)
+        lonResText = findViewById(R.id.lonResText)
+        latResText = findViewById(R.id.latResText)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        if (checkLocationPermission()) {
+            requestLocationUpdates()
+        }
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager: LocationManager =
+            getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+    private fun checkLocationPermission(): Boolean {
+        val permission = Manifest.permission.ACCESS_FINE_LOCATION
+        val granted = PackageManager.PERMISSION_GRANTED
+
+        if (ContextCompat.checkSelfPermission(this, permission) != granted) {
+            ActivityCompat.requestPermissions(this, arrayOf(permission), 1)
+            return false
+        }
+        return true
+    }
+
+    private fun requestLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+
+        fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+            if (location != null) {
+                val latitude = location.latitude
+                val longitude = location.longitude
+                lonResText.text = "$longitude"
+                latResText.text = "$latitude"
+            } else {
+                locationTextView.text = getString(R.string.error)
+            }
+        }
     }
 }
