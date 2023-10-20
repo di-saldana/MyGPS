5273345e7c56503412a2dfe9bc25c977bcbf1fe8
Dianelys Saldaña <dsl42@alu.ua.es>
2023-10-20 18:16:12 +0200

    Updated required permissions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "es.ua.eps.mygps"
-        minSdk = 33
+        minSdk = 26
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
--- app/src/main/java/es/ua/eps/mygps/MainActivity.kt
+++ app/src/main/java/es/ua/eps/mygps/MainActivity.kt
@@ -1,7 +1,9 @@
 package es.ua.eps.mygps
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -10,9 +12,11 @@ import android.telephony.gsm.GsmCellLocation
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         //  Telephony Manager & GmsCellLocation
         val iccidText = findViewById<TextView>(R.id.iccidText)
         val imsiText = findViewById<TextView>(R.id.imsiText)
@@ -25,6 +29,22 @@ class MainActivity : AppCompatActivity() {
         val telephonyManager = getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
         val cellLocation = telephonyManager.cellLocation as GsmCellLocation
 
+        val networkType = telephonyManager.networkType
+        networkTypeText.text = "Network Type: $networkType"
+
+        val imei = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            telephonyManager.imei
+        } else {
+            imeiText.text = "IMEI (Device ID): $--"
+        }
+        imeiText.text = "IMEI (Device ID): $imei"
+
+        val cellId = cellLocation.cid
+        cellIdText.text = "Cell ID: $cellId"
+
+        val lac = cellLocation.lac
+        lacText.text = "LAC (Location Area Code): $lac"
+
         val simSerialNumber = telephonyManager.simSerialNumber
         iccidText.text = "ICCID (Sim Serial Number): $simSerialNumber"
 
@@ -33,17 +53,5 @@ class MainActivity : AppCompatActivity() {
 
         val operatorName = telephonyManager.networkOperatorName
         operatorNameText.text = "Operator Name: $operatorName"
-
-        val networkType = telephonyManager.networkType
-        networkTypeText.text = "Network Type: $networkType"
-
-        val imei = telephonyManager.imei
-        imeiText.text = "IMEI (Device ID): $imei"
-
-        val cellId = cellLocation.cid
-        cellIdText.text = "Cell ID: $cellId"
-
-        val lac = cellLocation.lac
-        lacText.text = "LAC (Location Area Code): $lac"
     }
 }
